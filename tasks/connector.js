c15b1d452792d4c90410ce0224b88d18392f1b25
Trevor Sheridan <trevor@trevor.sh>
2013-06-30 13:23:27 -0600

    Working version of connector without less compilation

--- tasks/connector.js
+++ tasks/connector.js
@@ -10,47 +10,32 @@
 
 module.exports = function(grunt) {
   var utility = require('../lib/utility');
-  var less = require('../lib/less');
   
-  var express = require('express');
-  var engines = require('consolidate');
-  var app = express();
-  
-  grunt.registerMultiTask('connector', 'Prototyping server for the modern man.', function() {
-    app.engine('html', engines.hogan);
-    app.set('views', __dirname + '/application/views/');
-    app.set('view engine', 'html');
-    app.set('static', __dirname + '/application/static');
+  grunt.registerTask('connector', 'Prototyping server for the modern man.', function() {
+    var express = require('express');
+    var engines = require('consolidate');
+    var server = express();
     
-    // recompile assets if the environment is set to development
-    if (process.env.NODE_ENV === 'development') {
-      app.use(function(request, response, next) {
-        var media = request.headers.accept;
-        
-        if (media.match(/text\/css/)) {
-          // initialize a new less object with the source and compiled directories, then compile!
-          var css = Object.create(less).init(app.get('static') + '/source/styles', app.get('static') + '/source/public');
-          css.compile(css.source(request.url), function(css) {
-            !css ? next() : response.end(css);
-          });
-          
-          return false;
-        }
-        next();
-      });
-    }
+    var options = this.options({ port: process.env.PORT || 3000, base: process.cwd() + '/public', source: process.cwd() + '/source/views', templating: 'hogan' });
+    
+    // set the templating engine configuration
+    server.engine('html', engines[options.templating]);
+    server.set('views', options.source);
+    server.set('view engine', 'html');
     
     // serve static assets from the public directory (falls back to this if development is on and recompiling less)
-    app.use(express.static(__dirname + '/application/static/public'));
+    server.use(express.static(options.base));
     
     // catch all other requests and route to the appropriate template
-    app.use(function(request, response) {
+    server.use(function(request, response) {
       // get the directory listing for views, split the path into an array of components, index is the directory index.
-      var template = utility.find(utility.listing(app.get('views')), request._parsedUrl.pathname.split('/'), 'index');
-      response.status(template ? 200 : 404).render(template || '404');
+      var template = utility.find(utility.listing(server.get('views')), request._parsedUrl.pathname.split('/'), 'index');
+      var partials = { header: "a header", footer: "the footer" };
+      
+      response.status(template ? 200 : 404).render(template || '404', partials);
     });
     
-    // app is setup, start listening!
-    app.listen(process.env.PORT || 3000);
+    // server is setup, start listening!
+    server.listen(options.port);
   });
 };
